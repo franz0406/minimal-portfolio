b305e83c0d09d6e712dd03337ad9980702163128
hwang <franz0406@naver.com>
2022-03-11 02:31:13 +0900

    portfolio, contact, about page markup finish

--- js/main.js
+++ js/main.js
@@ -11,4 +11,23 @@ $(function(){
         $(this).parent().addClass('active');
     });
 
+    // isotope filter
+    const portFolio = document.querySelector('.portfolio');
+    if(!portFolio === false) { // 필터 적용시킬 대상 확인 조건문
+        const iso = new Isotope( portFolio, {
+            itemSelector: '.portfolio-item',
+            layoutMode: 'fitRows'
+        });
+    
+        const filtersElem = document.querySelector('.portfolio-filter');
+        
+        filtersElem.addEventListener( 'click', function( event ) {
+            // only work with buttons
+            if ( !matchesSelector( event.target, 'button' ) ) {
+                return;
+            }
+            const filterValue = event.target.getAttribute('data-filter');
+            iso.arrange({ filter: filterValue });
+        });
+    }
 });
---
+++ readme.md
@@ -0,0 +1,17 @@
+### HTML Attribute 우선순위
+
+
+Attribute의 우선순위는 다음 순위를 따른다.
+|순서|속성|
+|----|----|
+|1|rel|
+|2|type|
+|3|href, src|
+|4|width, height|
+|5|target|
+|6|id|
+|7|name|
+|8|class|
+|9|style|
+|10|title, alt|
+|11|기타 attribute|
